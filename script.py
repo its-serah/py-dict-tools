e3a95e7c9ce62f4f65b2e3c9a50e41e6335fdaa8
its-serah <serah.elrashidi@gmail.com>
2025-03-01 23:08:16 +0200

    Implemented Dictionaries Merger Function

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 class1_info = {
     "section" : "A",
     "level" : "Graduate",
-    "floor" : 3,
+    "lab" : "yes",
     "student_count": 24,  #camel case naming 
 }
 
@@ -15,5 +15,14 @@ class2_info = {
 
 #Dictionaries Merger Function implementation
 
+def merge_dictionaries (class1_info,class2_info):
+    class3_info = {}
+    # Copy class1_info into class3_info
+    for key in class1_info:
+        class3_info[key] = class1_info[key]
 
+    # Copy class2_info into class3_info 
+    for key in class2_info:
+        class3_info[key] = class2_info[key]  
 
+    return class3_info   
