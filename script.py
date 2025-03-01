8fc8e5d6f9efba39e26c31849994cffc2b91a638
its-serah <serah.elrashidi@gmail.com>
2025-03-01 22:48:11 +0200

    Added Global Variables

---
+++ script.py
@@ -0,0 +1,19 @@
+#Global variables
+class1_info = {
+    "section" : "A",
+    "level" : "Graduate",
+    "floor" : 3,
+    "student_count": 24,  #camel case naming 
+}
+
+class2_info = {
+    "section" : "C",
+    "level" : "undergraduate",
+    "floor" : 3,
+    "student_count": 14,  #camel case naming 
+}
+
+#Dictionaries Merger Function implementation
+
+
+
